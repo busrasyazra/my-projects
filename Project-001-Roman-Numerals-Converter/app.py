081907ed62d7f06801cb984c3f78587493e19883
busrasyazra <81545300+busrasyazra@users.noreply.github.com>
2021-07-27 18:33:43 -0700

    cfn-template added

--- Project-001-Roman-Numerals-Converter/app.py
+++ Project-001-Roman-Numerals-Converter/app.py
@@ -20,5 +20,5 @@ def main_post():
     else:
         return render_template('index.html', developer_name='Busra', not_valid=False)
 if __name__=='__main__':
-    app.run(debug=True)
-    #app.run(host='0.0.0.0', port=80)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
